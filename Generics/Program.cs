8bb2cb3525f1fd870f839d403a4ed70d96ed7874
mimamulhassankhan <pappuhassan440@gmail.com>
2020-11-23 11:30:15 +0600

    initial production

---
+++ Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = Calculator.areEqual<string>("A", "2");
+            Console.WriteLine(res);
+
+            var genericClassRes = GenericCalculator<string>.areEqual("A", "A");
+            Console.WriteLine(genericClassRes);
+        }
+    }
+
+    public class Calculator
+    {
+        public static bool areEqual<T>(T a, T b)
+        {
+            return a.Equals(b);
+        }
+    }
+
+    public class GenericCalculator<T>
+    {
+        public static bool areEqual(T a, T b)
+        {
+            return a.Equals(b);
+        }
+    }
+}
